b6c9853d9b3f25f8789590ff1741bdd488a2f4cf
Felipe <105891789+FelipeMDB-UNESP@users.noreply.github.com>
2023-05-26 20:09:28 -0300

    adição classe Produto

---
+++ Exercicio2/src/main/java/br/unesp/rc/exercicio2/Exercicio2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package br.unesp.rc.exercicio2;
+
+/**
+ *
+ * @author aluno
+ */
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Exercicio2/src/main/java/br/unesp/rc/exercicio2/modelo/Produto.java
@@ -0,0 +1,18 @@
+package br.unesp.rc.exercicio2.modelo;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class Produto {
+    
+    long codigo;
+    String nome;
+    double preco;
+    double desconto;
+}
